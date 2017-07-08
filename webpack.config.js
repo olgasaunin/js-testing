157fea5de3b1f5dbfddef5d729bee98bb5a501fb
Olga Saunin <olga.saunin@gmail.com>
2017-07-07 18:43:34 -0700

    deb abd prod

--- webpack.config.js
+++ webpack.config.js
@@ -22,123 +22,134 @@ const minifyPlugin = new webpack.LoaderOptionsPlugin({
   debug: false
 })
 
-module.exports = {
-  entry: {
-    index: [
-      //path.resolve(__dirname, 'app/index.pug'),
-      path.resolve(__dirname, 'app/index.js'),
-      path.resolve(__dirname, 'app/sass/main.scss')
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[chunkhash].js',
-    //publicPath: '/dist'
-  },
-  devtool: "cheap-module-eval-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets:  [
-                [ 'es2015', { modules: false } ],
-                [ 'es2017' ]
-              ],
-              plugins: ['transform-runtime', 'transform-decorators-legacy', 'transform-class-properties', 'transform-object-rest-spread']
-            }
-          },
-          {
-            loader: 'eslint-loader'
-          }
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: extractPlugin.extract({
+module.exports = (env = {}) => {
+  // Variables set by npm scripts in package.json
+  const isProduction = env.production === true
+
+  return {
+    entry: {
+      index: [
+        //path.resolve(__dirname, 'app/index.pug'),
+        path.resolve(__dirname, 'app/index.js'),
+        path.resolve(__dirname, 'app/sass/main.scss')
+      ]
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.[chunkhash].js',
+    },
+
+    devtool: (() => {
+      // return (isProduction) ? 'hidden-source-map' : 'cheap-module-eval-source-map'
+      return (isProduction) ? 'source-map' : 'cheap-module-eval-source-map'
+    })(),
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
           use: [
             {
-              loader: "css-loader",
+              loader: 'babel-loader',
               options: {
-                sourceMap: true
+                presets:  [
+                  [ 'es2015', { modules: false } ],
+                  [ 'es2017' ]
+                ],
+                plugins: ['transform-runtime', 'transform-decorators-legacy', 'transform-class-properties', 'transform-object-rest-spread']
               }
             },
             {
-              loader: "postcss-loader",
-              options: {
-                sourceMap: 'inline'
+              loader: 'eslint-loader'
+            }
+          ]
+        },
+        {
+          test: /\.scss$/,
+          use: extractPlugin.extract({
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: "postcss-loader",
+                options: {
+                  sourceMap: 'inline'
+                }
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true
+                }
               }
-            },
+            ]
+          })
+        },
+        {
+          test: /\.html$/,
+          exclude: /node_modules/,
+          use: ['html-loader']
+        },
+        {
+          test: /\.pug$/,
+          exclude: /node_modules/,
+          use: [{ loader: 'html-loader' },
+          { loader: 'pug-html-loader',
+            options: {
+              name: '[name],[ext]'
+            }
+          }
+          ]
+        },
+        {
+          test: /\.(jpg|png)$/,
+          exclude: /node_modules/,
+          use: [
             {
-              loader: "sass-loader",
+              loader: 'file-loader',
               options: {
-                sourceMap: true
+                name: '[name],[ext]',
+                outputPath: 'images/',
+                // publicPath: 'images/'
               }
             }
           ]
-        })
-      },
-      {
-        test: /\.html$/,
-        exclude: /node_modules/,
-        use: ['html-loader']
-      },
-      {
-        test: /\.pug$/,
-        exclude: /node_modules/,
-        use: [{ loader: 'html-loader' },
-        { loader: 'pug-html-loader',
-          options: {
-            name: '[name],[ext]'
-          }
         }
-        ]
-      },
-      {
-        test: /\.(jpg|png)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name],[ext]',
-              outputPath: 'images/',
-              // publicPath: 'images/'
-            }
-          }
-        ]
+      ]
+    },
+    plugins: [
+      extractPlugin,
+      providerPlugin,
+      cleanWebPackPlugin,
+      //uglifyJsPlugin,
+      new HtmlWebpackPlugin({
+        favicon: 'app/favicon.png',
+        template: 'app/index.pug',
+        filename: 'index.html',
+        chunk: ['index']
+      }),
+      new HtmlWebpackPlugin({
+        favicon: 'app/favicon.png',
+        template: 'app/service.pug',
+        filename: 'service.html',
+        chunk: ['index']
+      }),
+      minifyPlugin
+    ],
+    devServer: {
+      host: 'localhost',
+      port: 7000,
+      open: true,
+      historyApiFallback: true,
+      watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
       }
-    ]
-  },
-  plugins: [
-    extractPlugin,
-    providerPlugin,
-    cleanWebPackPlugin,
-    //uglifyJsPlugin,
-    new HtmlWebpackPlugin({
-      template: 'app/index.pug',
-      filename: 'index.html',
-      chunk: ['index']
-    }),
-    new HtmlWebpackPlugin({
-      template: 'app/service.pug',
-      filename: 'service.html',
-      chunk: ['index']
-    }),
-    minifyPlugin
-  ],
-  devServer: {
-    host: 'localhost',
-    port: 7000,
-    open: true,
-    historyApiFallback: true,
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000
     }
   }
 };
